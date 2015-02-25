7c03dba9823788f282e7a92f084ad7504be0f143
brunacruz11 <cruzbruna@gmail.com>
2015-02-25 02:20:08 -0800

    Add the old nudge.js

--- Chapter3/Amazeriffic/nudgeOld.js
+++ Chapter3/Amazeriffic/nudgeOld.js
@@ -21,17 +21,12 @@ function beginPage(res, title) {
     res.write("<head>\n");
     res.write("<meta charset='utf-8'>\n");
     res.write("<title>"+ title + "</title>\n");
-    res.write("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css'>\n");
-    res.write("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css'>\n");
-    res.write("<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>\n");
-    res.write("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js'></script>\n");
+    res.write("<link rel='stylesheet' href='style.css' type='text/css'>\n");
     res.write("</head>\n");
     res.write("<body>\n");
 }
 
 function endPage(res) {
-    endMain(res);
-    writeContainer(res);
     res.write("</body>\n");
     res.write("</html>\n");
     res.end();
@@ -39,59 +34,7 @@ function endPage(res) {
 
 function writeHeading(res, tag, title) {
     res.write("<" + tag + ">" + title + "</" + tag + ">\n");
-
 }
-function writeMenu(res) {
-    res.write("<nav class='navbar navbar-inverse navbar-fixed-top'>\n");
-    res.write("<div class='container'>\n");
-    res.write("<div class='navbar-header'>\n");
-    res.write("<a class='navbar-brand' href='#'>Nudge - Web Interface for Git Push</a>\n");
-    res.write("</div>\n");
-    res.write("</div>\n");
-    res.write("</nav>\n");
-
-}
-
-function writeMain(res) {
-    res.write("<div class='jumbotron'>\n");
-    res.write("<div class='container'>\n");
-    res.write("<h1>Nudge!</h1>\n");
-
-}
-
-function endMain(res) {
-    res.write("</div>\n");
-    res.write("</div>\n");
-
-}
-
-function writeContainer(res) {
-    res.write("<div class='container'>\n");
-    res.write("<div class='row'>\n");
-    res.write("<div class='col-md-4'>\n");
-    res.write("<h2>Github</h2>\n");
-    res.write("<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>\n");
-    res.write("<p><a class='btn btn-default' href='http://github.com/brunacruz11' role='button'>View details &raquo;</a></p>\n");
-    res.write("</div>\n");
-    res.write("<div class='col-md-4'>\n");
-    res.write("<h2>CPSC 473</h2>\n");
-    res.write("<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>\n");
-    res.write("<p><a class='btn btn-default' href='https://sites.google.com/site/cpsc473/'  role='button'>View details &raquo;</a></p>\n");
-    res.write("</div>\n");
-    res.write("<div class='col-md-4'>\n");
-    res.write("<h2>Assignment</h2>\n");
-    res.write("<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>\n");
-    res.write("<p><a class='btn btn-default' href='https://docs.google.com/a/csu.fullerton.edu/document/d/1ESiHyYyL1NRUDILRyHJPviitZJ5GNlfpRvohUumafHs/edit' role='button'>View details &raquo;</a></p>\n");
-    res.write("</div>\n");
-    res.write("</div>\n");
-    res.write("<hr>\n");
-    res.write("<footer>\n");
-    res.write("<p>&copy; Bruna Cruz 2015</p>\n");
-    res.write("</footer>\n");
-    res.write("</div>\n"); /* /container --> */
-
-}
-
 
 function writePre(res, divClass, data) {
     var escaped = data.replace(/</, "&lt;").
@@ -105,14 +48,11 @@ function writePre(res, divClass, data) {
 }
 
 function beginForm(res) {
-    res.write("<form method='POST' action='/push' class='form-inline'>\n");
+    res.write("<form method='POST' action='/push'>\n");
 }
 
 function endForm(res) {
-    /*<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>*/
-    /*res.write("<div>\n");*/
-    res.write("<input class='btn btn-primary' type='submit' value='Push'>\n");
-    /*res.write("</div>\n");*/
+    res.write("<input type='submit' value='Push'>\n");
     res.write("</form>\n");
 }
 
@@ -121,11 +61,9 @@ function capitalize(str) {
 }
 
 function beginSelect(res, what) {
-    /*res.write("<div class='" + what + "_div'>\n");*/
-    res.write("<div class='form-group'>\n");
+    res.write("<div class='" + what + "_div'>\n");
     res.write("<label for='" + what + "_select'>" + capitalize(what) + "</label>\n");
-    /*res.write("<div class='col-sm-6'>\n");*/
-    res.write("<select class='form-control' id='" + what + "_select' name='" + what + "'>\n");
+    res.write("<select id='" + what + "_select' name='" + what + "'>\n");
 }
 
 function writeOption(res, option) {
@@ -238,22 +176,15 @@ function frontPage(req, res) {
         var title = "Nudge - Web Interface for Git Push";
 
         beginPage(res, title);
-        /*writeHeading(res, "h1", title);*/
-        writeMenu(res);
-        writeMain(res);
+        writeHeading(res, "h1", title);
 
         if (req.method === "POST" && req.url === "/push") {
             gitPush(req, res);
         } else {
             gitStatus(res);
         }
-
     }
-
 }
-
-
-
 
 var server = http.createServer(frontPage);
 server.listen();
