cfaefc2b07b77b97f64e8d56a8c4faa9289f571d
brunacruz11 <cruzbruna@gmail.com>
2015-03-04 23:30:20 -0800

    adding files done until step 7

--- Chapter4/Amazeriffic/javascripts/app.js
+++ Chapter4/Amazeriffic/javascripts/app.js
@@ -1,62 +1,119 @@
-var main = function () {
-    var toDos = ["Get groceries",
-                 "Make up some new ToDos",
-                 "Prep for Monday's class",
-                 "Answer emails",
-                 "Take Gracie to the park",
-                 "Finish writing this book"];
+/* jshint browser: true, jquery: true, curly: true, eqeqeq: true, forin: true, immed: true, indent: 4, latedef: true, newcap: true, nonew: true, quotmark: double, strict: true, undef: true, unused: true */
 
-    $(".tabs a span").toArray().forEach(function (element) {
+/*var main = function() {
+    "use strict";
+    
+    var makeTabActive = function (tabNumber) {
+        // make all the tabs inactive
+        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+        $(".tabs span").removeClass("active");
+        $(tabSelector).addClass("active");
+        $("main .content").empty();
+    };
+
+    $(".tabs a:nth-child(1)").on("click", function () { 
+        makeTabActive(1);
+        return false;
+    });
+
+    $(".tabs a:nth-child(2)").on("click", function () { 
+        makeTabActive(2);
+        return false;
+    });
+
+    $(".tabs a:nth-child(3)").on("click", function () { 
+        makeTabActive(3);
+        return false;
+    });
+
+};*/
+
+/*Ask about the spaces on the JSLint*/
+
+var main = function () { 
+    "use strict";
+
+    var toDos = [
+        "Finish writing this book", 
+        "Take Gracie to the park", 
+        "Answer emails",
+        "Prep for Monday's class", 
+        "Make up some new ToDos", 
+        "Get Groceries"
+    ];
+
+
+    $(".tabs a span").toArray().forEach(function (element) { 
+    // create a click handler for this element 
+        $(element).on("click", function () {
+        // since we're using the jQuery version of element, 
+        // we'll go ahead and create a temporary variable 
+        // so we don't need to keep recreating it
         var $element = $(element);
+        var $content;
+        var $button;
+        var $input;    
+        
+        $(".tabs a span").removeClass("active"); 
+        $(element).addClass("active");
+        $("main .content").empty();
 
-        // create a click handler for this element
-        $element.on("click", function () {
-            var $content,
-                $input,
-                $button,
-                i;
+        if ($(element).parent().is(":nth-child(1)")) { 
+            $content = $("<ul>");
+            var index;
+            for(index = toDos.length - 1; index >= 0 ; index = index - 1) {
+                $content.append($("<li>").text(toDos[index]));
 
-            $(".tabs a span").removeClass("active");
-            $element.addClass("active");
-            $("main .content").empty();
-
-            if ($element.parent().is(":nth-child(1)")) {
-                // newest first, so we have to go through
-                // the array backwards
-                $content = $("<ul>");
-                for (i = toDos.length-1; i >= 0; i--) {
-                    $content.append($("<li>").text(toDos[i]));
-                }
-            } else if ($element.parent().is(":nth-child(2)")) {
-                // oldest first, so we go through the array forwards
-                $content = $("<ul>");
-                toDos.forEach(function (todo) {
-                    $content.append($("<li>").text(todo));
-                });
-            } else if ($element.parent().is(":nth-child(3)")) {
-                // input a new to-do
-                $input = $("<input>"),
-                $button = $("<button>").text("+");
-
-                $button.on("click", function () {
-                    if ($input.val() !== "") {
-                        toDos.push($input.val());
-                        $input.val("");
-                    }
-                });
-
-                $content = $("<div>").append($input).append($button);
-               /* Alternatively append() allows multiple arguments so the above
-                can be done with $content = $("<div>").append($input, $button); */
             }
-
             $("main .content").append($content);
 
-            return false;
-        });
+        } else if ($element.parent().is(":nth-child(2)")) { 
+            $content = $("<ul>");
+            toDos.forEach(function (todo) {
+                $content.append($("<li>").text(todo));
+            }); 
+            $("main .content").append($content);
+
+        } else if ($element.parent().is(":nth-child(3)")) { 
+            console.log("THIRD TAB CLICKED!");
+            $input = $("<input>");
+            $button = $("<button>");
+            $("main .content").append($input);
+            $("main .content").append($button.text("+"));
+            
+            var addActivityFromInputBox = function () {
+                
+                if($("main .content input").val() !== "") {
+                    toDos.push($("main .content input").val());
+                    $("main .content input").val("");
+                }
+            }; 
+            
+            $("main .content button").on("click", function () {
+                addActivityFromInputBox();
+            });
+        
+
+        }
+
+        return false;
+
+        
+        }); 
     });
 
     $(".tabs a:first-child span").trigger("click");
 };
-
 $(document).ready(main);
+
+
+
+
+
+
+
+
+
+
+
+
